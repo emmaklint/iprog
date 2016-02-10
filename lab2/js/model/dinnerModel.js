9c08f2350ee4a131a6f8d41af6d93211a65aa848
runhem <alexandra@ep.se>
2016-02-10 15:30:22 +0100

    tog bort var numberofguests=2

--- lab2/js/model/dinnerModel.js
+++ lab2/js/model/dinnerModel.js
@@ -3,7 +3,6 @@ var DinnerModel = function() {
  
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
-	var numberofguests= 2;
 
 	var menu={'starter': [],'main' : [], 'dessert':[]};
 
