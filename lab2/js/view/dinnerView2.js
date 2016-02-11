47663e3aca543c7513bca6f006dfed4ace11fdf8
runhem <alexandra@ep.se>
2016-02-11 13:51:42 +0100

    no message

--- lab2/js/view/dinnerView2.js
+++ lab2/js/view/dinnerView2.js
@@ -1,9 +1,10 @@
 var DinnerView2 = function (container,model) {
 
 	this.numberOfGuests = container.find("#numberOfGuests");
+	this.price = container.find("#dinnerCost");
 
 	this.numberOfGuests.html(model.getNumberOfGuests());
-
+	this.price.html(model.getTotalMenuPrice());
 
 
 }
